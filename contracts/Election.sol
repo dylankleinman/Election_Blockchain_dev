d4872014c3756063b1a2797a10e69b014d351336
Dylan Kleinman <dylankkleinman@gmail.com>
2021-06-15 16:12:40 -0700

    have front end capabilites working

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,28 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election{
-    //store candidate
-    //read candidate
-    string public candidate;
+    //Candidate model
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //Use a mapping (like map in JS) to create an array with key mapping for each candidate
+    mapping(uint => Candidate) public candidates;
+
+    //store candidates count (cant count a mapping so need to store value)
+    uint public candidatesCount;
+
     //Constructor is ran when deployed to blockchain
     constructor () public {
-        candidate = "Candidate 1"; 
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    //function to allow adding candidate name and increasing count
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
